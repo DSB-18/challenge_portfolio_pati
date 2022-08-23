42c07d360c0055800bff944da3a8653e57f8e751
DSB9 <boboshko.dmitriy@gmail.com>
2022-08-24 02:55:31 +0300

    3/4 of 3rd task and some code about 4th subtask

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,17 +1,25 @@
+import time
+
 from pages.base_page import BasePage
 
 
 class Dashboard(BasePage):
-    header_xpath = "// *[ @ id = "__next"] / div[1] / header"
-    main_page_button_xpath = '//*[text() = 'Main page']'
-    players_button_xpath = '//*[text() = 'Players']'
-    language_button_xpath = '//*[text() = 'Polski']'
-    sign_out_button_xpath = '//*[text() = 'Sign out']'
-    players_count_widget_xpath = '//*[text() = 'Players count']'
-    matches_count_widget_xpath = '//*[text() = 'Matches count']'
-    reports_Count_widget_xpath = '//*[text() = 'Reports count']'
-    events_count_widget_xpath = '//*[text() = 'Events count']'
-    activity_widget_xpath = '//*[text() = 'Players count']'
+    expected_title = "Scouts panel"
+    dashboard_url = 'https://scouts-test.futbolkolektyw.pl/'
+    # header_xpath = "// *[ @ id = "__next"] / div[1] / header"
+    # main_page_button_xpath = '//*[text() = 'Main page']'
+    # players_button_xpath = '//*[text() = 'Players']'
+    # language_button_xpath = '//*[text() = 'Polski']'
+    # sign_out_button_xpath = '//*[text() = 'Sign out']'
+    # players_count_widget_xpath = '//*[text() = 'Players count']'
+    # matches_count_widget_xpath = '//*[text() = 'Matches count']'
+    # reports_Count_widget_xpath = '//*[text() = 'Reports count']'
+    # events_count_widget_xpath = '//*[text() = 'Events count']'
+    # activity_widget_xpath = '//*[text() = 'Players count']'
 # For now it's ten and not with best selectors, but I promise to add ALL and with better selectors
     pass
 # "pass" line should be deleted according to video
+
+    def page_title(self):
+        time.sleep(5)
+        assert self.get_page_title(self.dashboard_url) == self.expected_title
--- pages/login_page.py
+++ pages/login_page.py
@@ -5,6 +5,22 @@ class LoginPage(BasePage):
     login_field_xpath = "//*[@id='login']"
     password_field_xpath = "//*[@id='password']"
     sign_in_button_xpath = "//*[@class='MuiButton-label']"
+    login_url = ('https://scouts-test.futbolkolektyw.pl/en')
+    expected_title = "Scouts panel - sign in"
+    box_title_xpath = "//*[@id='__next']/form/div/div[1]/h5"
+    box_header = 'Scouts panel'
 
     def type_in_email(self, email):
         self.field_send_keys(self.login_field_xpath, email)
+
+    def type_in_password(self, password):
+        self.field_send_keys(self.password_field_xpath, password)
+
+    def click_sign_in_button(self):
+        self.click_on_the_element(self.sign_in_button_xpath)
+
+    def page_title(self):
+        assert self.get_page_title(self.login_url) == self.expected_title
+
+    def check_title_of_header(self):
+        self.assert_element_text(self.driver, self.title_of_box_xpath, self.header_of_box)
